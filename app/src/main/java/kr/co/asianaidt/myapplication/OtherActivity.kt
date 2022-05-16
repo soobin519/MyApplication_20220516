411e44e0ea7cbea81cbe1f89ca89626de1026f8d
soobin519 <soobin0519@naver.com>
2022-05-16 16:52:35 +0900

    복귀 기능: finish로 현재 화면 종료

--- app/src/main/java/kr/co/asianaidt/myapplication/OtherActivity.kt
+++ app/src/main/java/kr/co/asianaidt/myapplication/OtherActivity.kt
@@ -1,11 +1,21 @@
 package kr.co.asianaidt.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+//            val myIntent =Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            //현재 화면 종료 -> 이전 화면이 다시 나타남
+            finish()
+        }
     }
 }
