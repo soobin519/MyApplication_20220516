4cf960f2765c42ca821dd34e36f38997bf1adf4b
soobin519 <soobin0519@naver.com>
2022-05-17 09:33:01 +0900

    intent 3 : 확인버튼 > 결과 설정 > 복귀

--- app/src/main/java/kr/co/asianaidt/myapplication/EditNicknameActivity.kt
+++ app/src/main/java/kr/co/asianaidt/myapplication/EditNicknameActivity.kt
@@ -1,11 +1,30 @@
 package kr.co.asianaidt.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnOk.setOnClickListener {
+
+            //1. 입력한 닉네임을 변수에 저장
+            val inputNickname = edtNickname.text.toString()
+
+            //2. 변수에 담은 닉네임을 돌아가는 길에 첨부 (intent활용)
+            val resultIntent = Intent() //단순 첨부용 => 출발/목적지 표시 x
+
+            resultIntent.putExtra("nickname", inputNickname)
+
+            //3. ok를 누른게 맞다고 추가 표시
+            setResult(RESULT_OK, resultIntent)
+
+            //4. 현재 화면을 종료 (복귀)
+            finish()
+        }
     }
 }
--- app/src/main/java/kr/co/asianaidt/myapplication/MainActivity.kt
+++ app/src/main/java/kr/co/asianaidt/myapplication/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //닉네임을 받으러 간다고 명시하는 코드 값
+    val REQ_CODE_NICKNAME = 10000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,6 +32,14 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("message", inputMessage)
 
             startActivity(myIntent)
+        }
+
+        btnEditNickname.setOnClickListener {
+            //1. intent 만드는 방법은 동일
+            val myIntent = Intent(this, EditNicknameActivity::class.java)
+
+            //2. 편도x, 왕복으로 간다고 명시
+            startActivityForResult(myIntent, REQ_CODE_NICKNAME)
         }
     }
 }
